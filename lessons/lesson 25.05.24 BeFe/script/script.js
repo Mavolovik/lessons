19834901e89fab3110078698344b66b86ecec221
Volodymyr Malenchak <164871800+Mavolovik@users.noreply.github.com>
2025-04-05 18:21:29 +0200

    #

---
+++ lessons/lesson 25.05.24 BeFe/script/script.js
@@ -0,0 +1,41 @@
+// for (start; end; step){
+//     body
+// }
+// let numbers = [-23, 1, 5, 0, 26, 4, 10, -4, 3, 5];
+
+// //вывести только четные положительные числа
+
+// // for(let i=0; i<numbers.length; i++){
+// //    if(numbers[i]%2==0 && numbers[i]>0)
+// //     console.log(numbers[i]);
+// // }
+
+// for(let i=0; i<numbers.length; i++){
+//     if(numbers[i]<0 || numbers[i]>10){
+//         console.log(numbers[i]);
+        
+//     }
+// }
+
+//заполнить массив числами от 0 до 10
+
+// let array =[];
+// for(let i=0;i<=10;i++){
+//     array.push(i);
+// }
+// console.log(array);
+
+// let array=[];
+// for(let i=0; i<=100; i+=2){
+//     array.push(i);
+// }
+// console.log(array);
+
+//Objects
+
+let array=[['john','bob','alex'],
+    [2000,2500,1500]];
+
+console.log(array);
+
+let obj ={}
